847634d46a3cd278d734c5707fd2688bc1c41135
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-02 19:33:41 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-length = len(args)
-sum = 0
-for i in range(length):
-    sum += int(args[i])
-print("{}".format(sum))
+if __name__ == "__main__":
+    length = len(args)
+    sum = 0
+    for i in range(length):
+        sum += int(args[i])
+    print("{}".format(sum))
