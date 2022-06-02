5a44d8c990157abb73b73dca670eba56ecca4528
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-02 18:46:04 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
+    length = len(sys.argv) - 1
     if length == 0:
         print("0 arguments.")
     elif length == 1:
