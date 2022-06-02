819064eccaa96d5a86fe3dc7155b89511d994912
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-02 18:42:59 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-length = len(argv)
-if length == 0:
-    print("0 arguments.")
-elif length == 1:
-    print("{} argument:".format(length))
-    print("{}: {}".format(argv[0]))
-elif length > 1:
-    print("{} arguments:".format(length))
-    for i in range(0,length + 1) :
-        print("{}: {}".format(argv[i]))
+if __name__ == "__main__":
+    length = len(argv)
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(argv[0]))
+    elif length > 1:
+        print("{} arguments:".format(length))
+        for i in range(0,length + 1) :
+            print("{}: {}".format(argv[i]))
