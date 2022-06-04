5d0e02d581adab85eb96e52e7ace7f8614ee90a7
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-04 22:14:19 +0300

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,9 +4,9 @@ def print_reversed_list_integer(my_list=[]):
         pass
     else:
         le = len(my_list)
-        reverseList =[]
+        reverseList = []
         for i in range(len(my_list)):
             le -= 1
-            reverseList.append(my_list[le]) 
+            reverseList.append(my_list[le])
         for j in range(len(reverseList)):
-            print("{:d}".format(int(reverseList[j])))
+            print ("{:d}".format(int(reverseList[j])))
