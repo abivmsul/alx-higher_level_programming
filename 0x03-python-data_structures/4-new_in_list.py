5c6a6c769c72cae8159dbbdb7702f7fc7f4c337c
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-04 22:41:19 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
     elif idx > len(my_list) - 1:
         return (my_list)
     else:
-        my_list[idx] = element
-        return (my_list)
+        newlist = my_list[:]
+        newlist[idx] = element
+        return (newlist)
