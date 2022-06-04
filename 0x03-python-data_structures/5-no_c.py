6a32bf9bdc6ebd03db0ca3fa69abedab04df8625
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-04 23:00:37 +0300

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            del my_string[i]
+            my_string.pop[i]
         else:
             continue
     return (my_string)
