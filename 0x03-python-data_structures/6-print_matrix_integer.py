7b814379730e1c3d000737670203b653b60fab2d
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-06-05 14:06:11 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,3 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}/n".format(my_list[i][j]))
+            if column != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
